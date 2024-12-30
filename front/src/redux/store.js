a4a8828980adba3ccbb2f3168b446c330a2c8920
Timothe Lebrun <timothe463@icloud.com>
2024-12-30 16:17:26 +0100

    refactor: remove useless code

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './slices/counterSlice.js';
 import authReducer from './slices/authSlice.js';
 export default configureStore({
     reducer: {
-        counter: counterReducer,
         auth: authReducer,
     }
 })
